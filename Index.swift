6692315cca29adac2cac15d7503173502fdc02f4
Pablo Groove <106037878+PabloGroove@users.noreply.github.com>
2022-07-14 23:36:22 +0200

    Create Index.swift

---
+++ Index.swift
@@ -0,0 +1,9 @@
+//To retrieve individual elements, we can use the subscript syntax, array[index]. The subscript syntax uses square brackets, [ ], around an index value.
+
+
+
+var temperature: [Int] = [66, 68, 72, 76, 80, 82, 85, 85, 84, 82, 81, 78, 74, 73, 72, 71, 70, 69, 68, 65, 63, 62, 61, 63]
+
+// code below 🌴
+
+print("The current temperature is \(temperature[0])°")
