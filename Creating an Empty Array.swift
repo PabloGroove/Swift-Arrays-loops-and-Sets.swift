26447a4b122ad62ad088ff08b555d1b14e48adf2
Pablo Groove <106037878+PabloGroove@users.noreply.github.com>
2022-07-14 23:21:51 +0200

    Create Creating an Empty Array.swift

---
+++ Creating an Empty Array.swift
@@ -0,0 +1,11 @@
+// Write your code below 🚇
+//Now that we’ve initialized the scores array, we can add integers (and only integers) inside.
+
+//Note: Arrays can only store items of the same data type. The type of the array cannot be changed after it’s declared.
+
+
+var subwayAdult = [Int]()
+print(subwayAdult)
+
+var subwayChild = [Int]()
+print(subwayChild)
